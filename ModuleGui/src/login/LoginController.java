b71db534e25f28bd2e7fd1a7a132733a75cbd306
George Vasiliu <george.vasiliu@certus.software>
2017-05-24 16:33:04 +0300

    updated graphics;

---
+++ ModuleGui/src/login/LoginController.java
@@ -0,0 +1,24 @@
+package login;
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by george on 5/24/17.
+ */
+public class LoginController {
+
+
+    @FXML
+    private TextField userNameField;
+    @FXML
+    private PasswordField userPasswordField;
+
+
+    @FXML
+    private void initialize() {
+
+    }
+}
